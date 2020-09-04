c595dfae66969a6d9df1ddcd7e30465b50579765
Fabian_Martinez <fama2091@gmail.com>
2020-09-04 11:23:08 -0500

    Hola Mundo cs

---
+++ Hola Mundo/Hola Mundo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hola_Mundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+
+            
+
+            Console.WriteLine("Ingrese el número 1");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el número 2");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine((n1 + n2));
+
+            Console.ReadKey();
+
+        }
+    }
+}
