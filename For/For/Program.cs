95bf09db7573a46753453925b5c65035031b115d
Husney <usneibayon@gmail.com>
2020-09-05 21:56:00 -0500

    For, While, Do While

---
+++ For/For/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace For
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			/*
+			 *	for(inicio; condición de fin; aumento/decremento){
+			 *	Haga...
+			 *	}
+			 */
+
+
+			for(int i = 0; i <= 10; i++)
+			{
+				Console.WriteLine(i);
+			}
+
+
+			Console.ReadKey();
+
+
+		}
+	}
+}
---
+++ ForEach/ForEach/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ForEach
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int []group = new int[5];
+
+			
+
+			for(int i = 0; i < group.Length; i++)
+			{
+
+
+				
+				
+			}
+		}
+	}
+}
