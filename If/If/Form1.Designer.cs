fb34cff1269509d640acc8f58831ea87178a7554
Fabian_Martinez <fama2091@gmail.com>
2020-09-05 19:56:03 -0500

    Operador ternario

---
+++ If/If/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace If
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(114, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ingresar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(137, 12);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(131, 20);
+            this.txtUser.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Usuario";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(138, 50);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(130, 20);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Contraseña";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 129);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ If/If/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace If
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+         string user = "user";
+         string pass = "password";
+
+            if (user.Equals(this.txtUser.Text))
+            {
+                MessageBox.Show("Usuario Correcto");
+
+                if (pass.Equals(this.txtPassword.Text)){
+                    MessageBox.Show("Contraseña Correcta");
+                }else
+                {
+                    MessageBox.Show("Contraseña Incorrecta");
+                }
+            }else
+            {
+                MessageBox.Show("Usuario Incorrecto");
+            }
+
+            var dia = 1;
+
+            switch (dia)
+            {
+                case 1:
+                    MessageBox.Show("Monday");
+                    break;
+
+                case 2:
+                    MessageBox.Show("Tuesday");
+                    break;
+
+                case 3:
+                    MessageBox.Show("Wednesday");
+                    break;
+
+                case 4:
+                    MessageBox.Show("Thrusday");
+                    break;
+
+                case 5:
+                    MessageBox.Show("Friday");
+                    break;
+
+                case 6:
+                    MessageBox.Show("Saturday");
+                    break;
+
+                case 7:
+                    MessageBox.Show("Sunday");
+                    break;
+
+                default:
+                    MessageBox.Show("Dia no valido");
+                    break;
+            }
+
+
+            //(condición) ? True : False ;
+
+            String numero = (5 > 3) ? "Es mayor a 5" : "Es menor a 5";
+
+            bool autenticado = true;
+
+//            (autenticado) ? Console.WriteLine("Autenticado") : Console.WriteLine("No autenticado");
+
+            Console.WriteLine((autenticado) ? "Autenticado" : "No autenticado");
+        }
+    }
+}
